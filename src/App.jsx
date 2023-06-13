5f9c865ae62f277ab21bfcd07231b79d1cc96a20
SametKiybik <samettemas05@gmail.com>
2023-06-13 09:32:47 +0300

    market

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
+import Navbarim from './Component/Navbar';
+import Fruits from './Pages/FruitsPage/Fruits.jsx';
+import Home from './Pages/HomePage/Home';
+import Vegetable from './Pages/VegetablePage/Vegetable';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarim/>
+      
+      <Routes>
+        <Route path='/Fruits' element={<Fruits/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Vegetable' element={<Vegetable/>} />
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import { Link } from "react-router-dom";
+
+
+
+function Navbarim() {
+
+  const imgStyle = {
+    width: '30px', // Örneğin, genişlik 100 piksel olsun
+    height: 'auto', // Yükseklik otomatik olarak ayarlanacak
+    
+  };
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#">
+          <Image src='https://cdn-icons-png.flaticon.com/128/3128/3128247.png' style={imgStyle}></Image>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0 w-100 justify-content-center"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link className='active'>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Fruits">Fruits</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/Vegetable">Vegetable</Link>
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navbarim
