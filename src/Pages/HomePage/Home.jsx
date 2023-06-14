8a9b1a3345ce1eade40ca6653e557e8f9ff62a76
SametKiybik <samettemas05@gmail.com>
2023-06-14 09:52:35 +0300

    h2

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css'
 import Container from 'react-bootstrap/Container';
+import { Row,Col } from 'react-bootstrap';
 
 
 
@@ -8,7 +9,15 @@ function Home() {
   
   return (
     <Container fluid className='p-0'>
-      <div  className='bgHome'>Home</div>
+      <div  className='bgHome'>
+        <Container>
+          <Row className='justify-content-end'>
+            <Col lg={10}>
+              <h2 className='htext'>TOKAT'IN İLK VE TEK ÜRÜN KARŞILAŞTIRMA SİTESİ</h2>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Container>
     
   )
