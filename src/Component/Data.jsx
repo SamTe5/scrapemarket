8e46b017416404927994ddef2deadfce91a01a6c
SametKiybik <samettemas05@gmail.com>
2023-06-18 23:25:32 +0300

    data new

--- src/Component/Data.jsx
+++ src/Component/Data.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import veriCek from '../puppeteer'
+import React, { useEffect, useState } from 'react';
 
+function MyComponent() {
+  const [data, setData] = useState([]);
 
-function Data() {
-    
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/data', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      });
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Veri çekme hatası:', error);
+    }
+  };
+
   return (
-    <div>Data</div>
-  )
+    <div>
+      {/* Veriyi kullanarak bileşenin render işlemlerini gerçekleştirin */}
+      {Object.entries(data).map(([fruit, markets]) => (
+        <div key={fruit}>
+          <h2>{fruit}</h2>
+          
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Data
+export default MyComponent;
