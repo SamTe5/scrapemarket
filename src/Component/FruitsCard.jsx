d71d2697ea164807210a3b3057768d2bfce84df4
SametKiybik <samettemas05@gmail.com>
2023-06-23 17:19:34 +0300

    cardDetail

--- src/Component/FruitsCard.jsx
+++ src/Component/FruitsCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap'
 
 function FruitsCard({productValue}) {
   console.log(productValue)
   return (
     <div>
         <h2>{productValue}</h2>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
     </div>
   )
 }
