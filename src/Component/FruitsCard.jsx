4ae389c18e1a33eb4048706d7103e242350a4bf2
SametKiybik <samettemas05@gmail.com>
2023-06-23 17:27:31 +0300

    VALUES

--- src/Component/FruitsCard.jsx
+++ src/Component/FruitsCard.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap'
+import Erenler from './Erenler';
+import Niktas from './Niktas';
+import Migros from './Migros';
+import Carrefoursa from './Carrefoursa';
+import A101 from './A101'
+import Sok from './Sok';
+
 
 function FruitsCard({productValue}) {
   console.log(productValue)
@@ -8,12 +15,24 @@ function FruitsCard({productValue}) {
     <div>
         <h2>{productValue}</h2>
         <Row>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
+          <Col>
+            <Erenler></Erenler>
+          </Col>
+          <Col>
+            <Niktas></Niktas>
+          </Col>
+          <Col>
+            <A101></A101>
+          </Col>
+          <Col>
+            <Sok></Sok>
+          </Col>
+          <Col>
+            <Migros></Migros>
+          </Col>
+          <Col>
+            <Carrefoursa></Carrefoursa>
+          </Col>
         </Row>
     </div>
   )
