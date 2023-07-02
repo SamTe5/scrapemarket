c1488187ba1395ca88466c7f0735b794ab113237
SametKiybik <samettemas05@gmail.com>
2023-07-02 08:38:38 +0300

    ıtems

---
+++ src/Component/LeftItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+
+
+function LeftItem() {
+  return (
+    <div>LeftItem</div>
+  )
+}
+
+export default LeftItem
