2a2fcc146b7ef0a39861e384ec43d864ff6470d7
SametKiybik <samettemas05@gmail.com>
2023-07-07 22:44:12 +0300

    url and name

--- src/Pages/HomePage/HomeSlider.jsx
+++ src/Pages/HomePage/HomeSlider.jsx
@@ -8,12 +8,15 @@ function HomeSlider() {
       <Row className='justify-content-end'>
         <Sliders 
           name="Fruits"
+          url="https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
         />
         <Sliders
           name="Vegetables"
+          url="https://images.unsplash.com/photo-1518390643573-66f352c5492e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
         />
         <Sliders
           name="Basic Foods"
+          url="https://images.unsplash.com/photo-1484325881845-65073528922e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80"
         />
         <Sliders
           name="Untitled"
