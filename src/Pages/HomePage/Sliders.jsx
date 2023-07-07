18e556eb48dea7d571ffa8a4b8a992b8c6538afb
SametKiybik <samettemas05@gmail.com>
2023-07-07 22:24:27 +0300

    slider props

---
+++ src/Pages/HomePage/Sliders.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+
+function Sliders(props) {
+    return (
+        <Col sm={2}>
+            <div className='text-center'>
+                <h2>{props.name}</h2>
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100"
+                            src={props.url}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{props.food}</h3>
+                            <p>{props.price}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src={props.url}
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{props.food2}</h3>
+                            <p>{props.price2}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={props.url}
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{props.food3}</h3>
+                            <p>
+                                {props.price3}
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </Col>
+    )
+}
+
+export default Sliders
