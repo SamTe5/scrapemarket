8fa94bff7d4e31bc58988ca47e2ac12deb9dea8d
SametKiybik <samettemas05@gmail.com>
2023-07-07 12:28:40 +0300

    first slider

--- src/Pages/HomePage/HomeSlider.jsx
+++ src/Pages/HomePage/HomeSlider.jsx
@@ -1,17 +1,58 @@
 import React from 'react'
-import { Row,Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 
 function HomeSlider(props) {
   return (
     <Col sm={10}>
       <Row className='justify-content-end'>
-        <Col sm={2}>İlk</Col>
+        <Col sm={2}>
+          <div>
+            <h2>Fruits</h2>
+            <Carousel>
+              <Carousel.Item interval={1000}>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                  alt="Third slide"
+                />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </Col>
         <Col sm={2}>ikinci</Col>
         <Col sm={2}>Üçüncü</Col>
         <Col sm={2}>Dördüncü</Col>
         <Col sm={2}>Beşinci</Col>
-      </Row>    
+      </Row>
     </Col>
   )
 }
