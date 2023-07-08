29621a57a2c2c9d5298c70b42ca20669f8ffe9f5
SametKiybik <samettemas05@gmail.com>
2023-07-08 09:35:44 +0300

    random values

--- src/Pages/HomePage/HomeSlider.jsx
+++ src/Pages/HomePage/HomeSlider.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Sliders from './Sliders';
 
 function HomeSlider() {
+  const [data, setData] = useState([]); 
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/data', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      });
+      const jsonData = await response.json();
+      const wrappedJsonData = [jsonData];
+      setData(wrappedJsonData);
+    } catch (error) {
+      console.error('Veri çekme hatası:', error);
+    }
+  };
+
+  const fH = data.length > 0 ? (Object.keys(data[0][0]).map((value, index) => {
+    const numbers=[]
+    let i=0
+    while(i<3){
+      const number=Math.floor(Math.random()*value.length)
+      numbers.push(number)
+      i++
+    }
+    console.log(numbers)
+  })) :(console.log("wait"))
+
+  
+  
+
+
   return (
     <Col sm={10}>
       <Row className='justify-content-end'>
-        <Sliders 
+
+        <Sliders
           name="Fruits"
           url="https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+
+
+
         />
         <Sliders
           name="Vegetables"
@@ -24,7 +65,7 @@ function HomeSlider() {
         <Sliders
           name="Untitled"
         />
-        
+
       </Row>
     </Col>
   )
