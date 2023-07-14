366c3f697316c51692a26630187b8c6ec23de1ca
SametKiybik <samettemas05@gmail.com>
2023-07-14 20:18:58 +0300

    props

--- src/Component/Sliders.jsx
+++ src/Component/Sliders.jsx
@@ -2,47 +2,47 @@ import React from 'react'
 import { Col } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 
-function Sliders(propsum) {
+function Sliders(props) {
     return (
         <Col sm={2}>
             <div className='text-center'>
-                <h3>{propsum.name}</h3>
+                <h3>{props.name}</h3>
                 <Carousel>
                     <Carousel.Item interval={1000}>
                         <img
                             className="d-block w-100"
-                            src={propsum.url}
+                            src={props.url}
                             alt="First slide"
                         />
                         <Carousel.Caption>
-                            <h5>{propsum.market}</h5>
-                            <h6>{propsum.food}</h6>
-                            <p>{propsum.price}</p>
+                            <h5>{props.market}</h5>
+                            <h6>{props.food}</h6>
+                            <p>{props.price}</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item interval={500}>
                         <img
                             className="d-block w-100"
-                            src={propsum.url}
+                            src={props.url}
                             alt="Second slide"
                         />
                         <Carousel.Caption>
-                            <h5>{propsum.market2}</h5>
-                            <h6>{propsum.food2}</h6>
-                            <p>{propsum.price2}</p>
+                            <h5>{props.market2}</h5>
+                            <h6>{props.food2}</h6>
+                            <p>{props.price2}</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={propsum.url}
+                            src={props.url}
                             alt="Third slide"
                         />
                         <Carousel.Caption className='pt-1'>
-                            <h5>{propsum.market3}</h5>
-                            <h6>{propsum.food3}</h6>
+                            <h5>{props.market3}</h5>
+                            <h6>{props.food3}</h6>
                             <p>
-                                {propsum.price3}
+                                {props.price3}
                             </p>
                         </Carousel.Caption>
                     </Carousel.Item>
