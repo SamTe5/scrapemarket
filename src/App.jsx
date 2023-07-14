aa33af5e2bb061042905e85ac3626dcf2537461e
SametKiybik <samettemas05@gmail.com>
2023-07-14 20:39:30 +0300

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbarim from './Component/Navbar';
 import Fruits from './Pages/FruitsPage/Fruits.jsx';
 import Home from './Pages/HomePage/Home';
 import Vegetable from './Pages/VegetablePage/Vegetable';
 import Basic from './Pages/BasicPage/Basic';
 import React, { useEffect, useState } from 'react';
-
 
 function App() {
   const [dataGeneral, setDatam] = useState([]);
@@ -32,25 +31,25 @@ function App() {
     }
   };
 
-
+  const renderContent = () => {
+    if (dataGeneral.length === 0) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <Routes>
+          <Route path='/Fruits' element={<Fruits data={dataGeneral} />} />
+          <Route path='/' element={<Home data={dataGeneral} />} />
+          <Route path='/Vegetable' element={<Vegetable data={dataGeneral} />} />
+          <Route path='/Basic' element={<Basic />} />
+        </Routes>
+      );
+    }
+  };
 
   return (
     <div className="App vh-100">
-      <Navbarim/>      
-      
-      <Routes>
-        <Route path='/Fruits' element={<Fruits
-          data={dataGeneral}
-        />}/>
-        <Route path='/Home' element={<Home
-          data={dataGeneral}
-        />}/>
-        <Route path='/Vegetable' element={<Vegetable
-          data={dataGeneral}
-        />} />
-        <Route path='/Basic' element={<Basic/>} />
-      </Routes>
-
+      <Navbarim />
+      {renderContent()}
     </div>
   );
 }
